dbf96600cead6f3d473bea55170775020959a63c
Sébastien Serre <sserre.bx@gmail.com>
2018-02-14 23:23:53 +0100

    modified API

--- mustache_api.h
+++ mustache_api.h
@@ -11,16 +11,15 @@
 /* Initialize a new store */
 extern TemplateStore* Mstc_template_create();
 
-/* Load file name, store it if not done, and return it.*/
-extern Template*
-Mstc_template_load(TemplateStore *store, const char *fileName);
+/* Free everything */
+extern void Mstc_template_free(TemplateStore *store);
 
-/* Get a template, same as Mstc_template_load*/
+/* Load file name, store it if not done, and return it.*/
 extern Template*
 Mstc_template_get(TemplateStore *store, const char *filename);
 
-/* Free everything */
-extern void Mstc_template_free(TemplateStore *store);
+/* Expand template with dict values */
+extern char* Mstc_expand(const Template*, const Dict*);
 
 
 /*******************************************************************************
@@ -31,10 +30,7 @@ extern void Mstc_template_free(TemplateStore *store);
 extern Dict* Mstc_dict_new();
 
  /* Set text value */
-extern void Mstc_dict_setValue(Dict *dict, char *key, const char *value);
-
-/* set formated key value like printf */
-extern void Mstc_dict_setFValue(Dict *dict, char *key, const char *format, ...);
+extern void Mstc_dict_setValue(Dict *dict, char *key, const char *format, ...);
 
 /* inconditionaly hide or show section */
 extern void Mstc_dict_setShowSection(Dict *dict, char *key, const bool show);
@@ -44,12 +40,6 @@ extern Dict* Mstc_dict_addSectionItem(Dict *dict, char *key);
 
 /* Free the main dictionnary.*/
 extern void Mstc_dict_free(Dict* dict);
-
-
-/*******************************************************************************
- * expand
- ******************************************************************************/
-extern char* Mstc_expand(const Template*, const Dict*);
 
 
 #endif /* _MUSTACHE_API_H_ */
--- tests.c
+++ tests.c
@@ -1,79 +1,38 @@
-#include "mustache.h"
+#include "mustache_api.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
 
-static char title_str[] = "title";
-KeyHash title;
-static char name_str[] = "name";
-KeyHash name;
-static char genre_str[] = "genre";
-KeyHash genre;
-static char page_link_str[] = "page_link";
-KeyHash page_link;
-static char author_str[] = "author";
-KeyHash author;
-static char url_str[] = "url";
-KeyHash url;
-static char pets_str[] = "pets";
-KeyHash pets;
-static char kind_str[] = "kind";
-KeyHash kind;
-static char footer_str[] = "footer";
-KeyHash footer;
-static char noescape_str[] = "noescape";
-KeyHash noescape;
-static char escape_str[] = "escape";
-KeyHash escape;
-static char childs_str[] = "childs";
-KeyHash childs;
-static char nest_str[] = "nest";
-KeyHash nest;
-
 static char testfile[] = "sample.html.tpl";
 
-void init_keyhash() {
-    title = Mstc_dict_genKeyHash(title_str);
-    name = Mstc_dict_genKeyHash(name_str);
-    genre = Mstc_dict_genKeyHash(genre_str);
-    page_link = Mstc_dict_genKeyHash(page_link_str);
-    author = Mstc_dict_genKeyHash(author_str);
-    url = Mstc_dict_genKeyHash(url_str);
-    pets = Mstc_dict_genKeyHash(pets_str);
-    kind = Mstc_dict_genKeyHash(kind_str);
-    footer = Mstc_dict_genKeyHash(footer_str);
-    noescape = Mstc_dict_genKeyHash(noescape_str);
-    escape = Mstc_dict_genKeyHash(escape_str);
-    childs = Mstc_dict_genKeyHash(childs_str);
-    nest = Mstc_dict_genKeyHash(nest_str);
-}
 
 Dict*
 simulate_request() {
     Dict *dict = Mstc_dict_new();
     Dict *sub, *subsub;
-    Mstc_dict_setValue2(dict, &title, "My wonderfull title");
-    Mstc_dict_setValue2(dict, &name, "jojo");
-    Mstc_dict_setValue2(dict, &genre, "male");
-    Mstc_dict_setValue2(dict, &page_link, "<a href=\"https://www.gg.fr\">gg</a>");
-    Mstc_dict_setValue2(dict, &author, "jojo");
-    Mstc_dict_setValue2(dict, &url, "http://jojo.fr");
+    Mstc_dict_setValue(dict, "title", "My wonderfull title");
+    Mstc_dict_setValue(dict, "name", "jojo");
+    Mstc_dict_setValue(dict, "genre", "male");
+    Mstc_dict_setValue(dict, "page_link", "<a href=\"https://www.gg.fr\">gg</a>");
+    Mstc_dict_setValue(dict, "author", "jojo");
+    Mstc_dict_setValue(dict, "url", "http://jojo.fr");
 
-    sub = Mstc_dict_addSectionItem2(dict, &pets);
-    Mstc_dict_setValue2(sub, &name, "gwen");
-    Mstc_dict_setValue2(sub, &kind, "chien");
+    sub = Mstc_dict_addSectionItem(dict, "pets");
+    Mstc_dict_setValue(sub, "name", "gwen");
+    Mstc_dict_setValue(sub, "kind", "chien");
 
-    sub = Mstc_dict_addSectionItem2(dict, &pets);
-    Mstc_dict_setValue2(sub, &name, "chuchen");
-    Mstc_dict_setValue2(sub, &kind, "chat");
-    subsub = Mstc_dict_addSectionItem2(sub, &nest);
-    Mstc_dict_setValue2(subsub, &name, "hello nest");
+    sub = Mstc_dict_addSectionItem(dict, "pets");
+    Mstc_dict_setValue(sub, "name", "chuchen");
+    Mstc_dict_setValue(sub, "kind", "chat");
 
-    Mstc_dict_setValue2(dict, &footer, "copyrithg jojo");
-    Mstc_dict_setValue2(dict, &noescape, "<a href=\"#\">jo</a>");
-    Mstc_dict_setValue2(dict, &escape, "<a href=\"#\">'jo'</a>");
+    subsub = Mstc_dict_addSectionItem(sub, "nest");
+    Mstc_dict_setValue(subsub, "name", "hello nest");
+
+    Mstc_dict_setValue(dict, "footer", "copyrithg jojo");
+    Mstc_dict_setValue(dict, "noescape", "<a href=\"#\">jo</a>");
+    Mstc_dict_setValue(dict, "escape", "<a href=\"#\">'jo'</a>");
 
     return dict;
 }
@@ -88,27 +47,29 @@ main(int argc, char **argv) {
 
     init_keyhash();
 
-    RessourceStore *store = Mstc_ressource_create();
-    Ressource *ressource = Mstc_ressource_load(store, testfile);
+    TemplateStore *store = Mstc_template_create();
+    Template *template = Mstc_template_load(store, testfile);
 
     int i;
-    Dict *d;
+    Dict *dict;
     char *output;
     for (i=0; i<count; i++) {
-        d = simulate_request();
-        output = Mstc_expand(ressource, d);
-        Mstc_dict_free(d);
+        dict = simulate_request();
+        output = Mstc_expand(template, dict);
+        Mstc_dict_free(dict);
         free(output);
     }
 
 
     /* one more to print out */
-    d = simulate_request();
-    output = Mstc_expand(ressource, d);
+    dict = simulate_request();
+    output = Mstc_expand(template, dict);
+    Mstc_dict_free(ddict
+
     printf("%s\n",output);
     free(output);
-    Mstc_dict_free(d);
-    Mstc_ressource_free(store);
+
+    Mstc_template_free(store);
 
     return 0;
 }
