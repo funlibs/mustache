8f842c6f6763415f7ad3adb7e11835aba4c985a6
Sébastien Serre <sserre.bx@gmail.com>
2018-02-14 21:47:32 +0100

    todo

--- mustache_api.h
+++ mustache_api.h
@@ -28,26 +28,22 @@ extern void Mstc_template_free(TemplateStore *store);
  ******************************************************************************/
 /* Will create a "main" dictionary. This is the one we will want to
  * free with Mstc_dict_free(). */
-Dict* Mstc_dict_new();
+extern Dict* Mstc_dict_new();
 
  /* Set text value */
-void Mstc_dict_setValue(Dict *dict, char *key, const char *value);
+extern void Mstc_dict_setValue(Dict *dict, char *key, const char *value);
 
 /* set formated key value like printf */
-void Mstc_dict_setFValue(Dict *dict, char *key, const char *format, ...);
+extern void Mstc_dict_setFValue(Dict *dict, char *key, const char *format, ...);
 
-/* setShowSection(false), will hide a sections (of the same name) that
- * contains some values and which would have been evaluated to true without
- * this explicit false.
- * showsection(true), will inconditionaly show the section, even if there is no
- * content in the section.*/
-void Mstc_dict_setShowSection(Dict *dict, char *key, const bool show);
+/* inconditionaly hide or show section */
+extern void Mstc_dict_setShowSection(Dict *dict, char *key, const bool show);
 
 /* do not free these dicts! */
-Dict* Mstc_dict_addSectionItem(Dict *dict, char *key);
+extern Dict* Mstc_dict_addSectionItem(Dict *dict, char *key);
 
 /* Free the main dictionnary.*/
-void Mstc_dict_free(Dict* dict);
+extern void Mstc_dict_free(Dict* dict);
 
 
 /*******************************************************************************
